661a6bcce5b7cbf08762b488c663eee1e35d4799
oneyellowflower <114711549+oneyellowflower@users.noreply.github.com>
2022-10-04 07:21:06 +0000

    연습

--- 포트폴리오_YG_P.js
+++ 포트폴리오_YG_P.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-    $(".o_ROSE").hover(function() {
-      $(".f_ROSE").toggleClass("on");
-    })
-});
+ $(document).ready(function() {
+    $(".box2").hover(function(){
+      $(this).children(".name").toggleClass("on");
+    });
+  });
+
+
